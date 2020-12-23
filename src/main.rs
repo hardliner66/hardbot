9613cb8a35319200f50b83944063d0abf346069f
hardliner66 <hardliner66@gmail.com>
2020-12-23 02:36:27 +0100

    clean up code add some commands

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,46 @@ use std::sync::mpsc::channel;
 use stringlit::s;
 use twitch_chat_wrapper::{run, ChatMessage};
 
+const COMMANDS: [&str; 11] = [
+    "!ping",
+    "!github | !git | !gh",
+    "!boss",
+    "!togglebit | !toggle",
+    "!catpeasant | !cat",
+    "!arcticspacefox | !arctic",
+    "!bob",
+    "!energy",
+    "!pog",
+    "!lul",
+    "!hype",
+];
+const ARCTIC: &str = "Look what @ArcticSpaceFox made: iamhar2Bob iamhar2Energy";
+const CATPEASANT: &str = "toggle8Catpeasant";
+const TOGGLEBIT: &str = "https://www.twitch.tv/togglebit";
+const GITHUB: &str = "https://github.com/hardliner66";
+
 fn main() -> anyhow::Result<()> {
     let (tx, rx) = channel::<String>();
     let (tx2, rx2) = channel::<ChatMessage>();
 
     let simple = hashmap! {
-        s!("!ping") => s!("pong"),
-        s!("!git") => s!("https://github.com/hardliner66"),
-        s!("!github")  => s!("https://github.com/hardliner66"),
-        s!("!gh") => s!("https://github.com/hardliner66"),
-        s!("!boss") => s!("toggle8Boss"),
-        s!("!toggle") => s!("https://www.twitch.tv/togglebit"),
-        s!("!togglebit") => s!("https://www.twitch.tv/togglebit"),
-        s!("!cat") => s!("toggle8Catpeasant"),
-        s!("!catpeasant") => s!("toggle8Catpeasant"),
-        s!("!arctic") => s!("Look what @ArcticSpaceFox made: iamhar2Bob iamhar2Energy"),
-        s!("!arcticspacefox") => s!("Look what @ArcticSpaceFox made: iamhar2Bob iamhar2Energy"),
-        s!("!bob") => s!("iamhar2Bob"),
-        s!("!energy") => s!("iamhar2Energy"),
-        s!("!pog") => s!("PogChamp"),
-        s!("!lul") => s!("LUL"),
-        s!("!commands") => s!("!ping\n!github | !git | !gh\n!boss\n!togglebit | !toggle\n!catpeasant | !cat\n!arcticspacefox | !arctic\n!bob\n!energy\n!pog\n!lul\n!hype")
+        s!("!ping") => "pong".to_string(),
+        s!("!git") => GITHUB.to_string(),
+        s!("!github")  => GITHUB.to_string(),
+        s!("!gh") => GITHUB.to_string(),
+        s!("!boss") => "toggle8Boss".to_string(),
+        s!("!toggle") => TOGGLEBIT.to_string(),
+        s!("!togglebit") => TOGGLEBIT.to_string(),
+        s!("!cat") => CATPEASANT.to_string(),
+        s!("!catpeasant") => CATPEASANT.to_string(),
+        s!("!arctic") => ARCTIC.to_string(),
+        s!("!arcticspacefox") => ARCTIC.to_string(),
+        s!("!bob") => "iamhar2Bob".to_string(),
+        s!("!energy") => "iamhar2Energy".to_string(),
+        s!("!pog") => "PogChamp".to_string(),
+        s!("!lul") => "LUL".to_string(),
+        s!("!lurk") => "Have fun lurking! iamhar2Bob".to_string(),
+        s!("!commands") => COMMANDS.join(" | "),
     };
 
     std::thread::spawn(move || {
@@ -41,14 +60,16 @@ fn main() -> anyhow::Result<()> {
                                 .collect::<Vec<_>>()
                                 .join(" "),
                         )
-                    } else if message.starts_with("!") && !message.chars().all(|c| c == '!'){
-                        Some(s!("No hacking allowed!"))
+                    } else if message.starts_with("!") && !message.chars().all(|c| c == '!') {
+                        Some(s!("No hacking allowed!\nUse !commands to see available commands."))
                     } else {
                         None
                     }
                 })
             } {
-                tx.send(response).unwrap();
+                for msg in response.split("\n") {
+                    tx.send(msg.to_owned()).unwrap();
+                }
             }
         }
     });
