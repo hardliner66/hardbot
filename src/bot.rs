8fe9139c67b4a0918d787bb48fba754c314113b8
hardliner66 <hardliner66@gmail.com>
2021-02-16 19:41:04 +0100

    get rid of unsafe impl

--- src/bot.rs
+++ src/bot.rs
@@ -13,11 +13,6 @@ pub struct Bot {
     pub runtime: Runtime,
 }
 
-// eww, don't do that
-// TODO: fix that properly!
-unsafe impl Send for Bot {}
-unsafe impl Sync for Bot {}
-
 impl Bot {
     pub fn new(config: Config) -> Self {
         Bot {
